adc6ebd1f51d27bb8a94190409c1a6c8a3d6dd84
Kartik <agni5kartik@gmail.com>
2026-05-29 00:08:39 +0530

    Day 7: Addition of important functions in DLL implementation

--- cpp-playground/03-data-structures/linear/linked-lists/doubly-linked-list/doubly-linked-list.cpp
+++ cpp-playground/03-data-structures/linear/linked-lists/doubly-linked-list/doubly-linked-list.cpp
@@ -37,6 +37,16 @@ class DoublyLinkedList {
         }
     }
 
+    int getLength() {
+        int length = 0;
+        Node *current = head;
+        while (current != nullptr) {
+            length++;
+            current = current->next;
+        }
+        return length;
+    }
+
     void append(int data) {
         Node *newNode = new Node(data);
         if (head == nullptr) {
@@ -57,6 +67,64 @@ class DoublyLinkedList {
             newNode->next = head;
             head = newNode;
         }
+    }
+
+    void insertAtPos(int data, int pos) {
+        if(pos < 0) {
+            cout << "Position cannot be negative." << endl;
+            return;
+        }
+        if(pos == 0) {
+            prepend(data);
+            return;
+        }
+        if(pos >= getLength()) {
+            append(data);
+            return;
+        }
+        Node *newNode = new Node(data);
+        Node *current = head;
+        for(int i = 0; i < pos; i++) {
+            current = current->next;
+        }
+        newNode->next = current;
+        newNode->prev = current->prev;
+        current->prev->next = newNode;
+        current->prev = newNode;
+    }
+
+    void deleteAtPos(int pos) {
+        if(pos < 0 || pos >= getLength()) {
+            cout << "Position out of bounds." << endl;
+            return;
+        }
+        Node *current = head;
+        for(int i = 0; i < pos; i++) {
+            current = current->next;
+        }
+        if(current->prev != nullptr) {
+            current->prev->next = current->next;
+        } else {
+            head = current->next;
+        }
+        if(current->next != nullptr) {
+            current->next->prev = current->prev;
+        } else {
+            tail = current->prev;
+        }
+        delete current;
+    }
+
+    int search(int data) {
+        Node* current = head;
+        int lengthOfList = getLength();
+        for(int i = 0; i < lengthOfList; i++) {
+            if(current->data == data) {
+                return i;
+            }
+            current = current->next;
+        }
+        return -1; // Data not found
     }
 
     void display() {
