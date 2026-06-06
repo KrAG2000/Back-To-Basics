7b24bd248d949fdd888d06e1504273ba1836d9b1
Kartik <agni5kartik@gmail.com>
2026-06-06 23:20:24 +0530

    - Day 11: LRU Cache get and put implementation

--- cpp-playground/05-projects/01-implementation-level/01-lru-cache/lru-cache.cpp
+++ cpp-playground/05-projects/01-implementation-level/01-lru-cache/lru-cache.cpp
@@ -1,62 +1,49 @@
 #include <iostream>
 #include <unordered_map>
-// #include <list>
+#include <list>
 
-class Node {
-    public:
-        int key;
-        int value;
-        Node* prev;
-        Node* next;
-
-        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
-};
-
-class DoublyLinkedList {
-    private:
-        Node* head;
-        Node* tail;
-
-    public:
-        DoublyLinkedList() : head(nullptr), tail(nullptr) {}
-
-        void addToFront(Node* node) {
-            node->next = head;
-            node->prev = nullptr;
-
-            if (head != nullptr) {
-                head->prev = node;
-            }
-            head = node;
-
-            if (tail == nullptr) {
-                tail = node;
-            }
-        }
-
-        void removeNode(Node* node) {
-            if (node->prev != nullptr) {
-                node->prev->next = node->next;
-            } else {
-                head = node->next;
-            }
-
-            if (node->next != nullptr) {
-                node->next->prev = node->prev;
-            } else {
-                tail = node->prev;
-            }
-        }
-
-        Node* removeTail() {
-            if (tail == nullptr) return nullptr;
-
-            Node* temp = tail;
-            removeNode(tail);
-            return temp;
-        }
-};
-
+using namespace std;
 class LRUCache {
-    
+    private:
+        list<pair<int, int>> cacheList; // List (Doubly linked list) to store key-value pairs
+        unordered_map<int, list<pair<int, int>>::iterator> cacheMap; // Hash map to store key and its corresponding iterator in the list
+    public:
+        int capacity;
+
+        LRUCache(int capacity) {
+            this->capacity = capacity;
+        }
+
+        int getValue(int id) {
+            if (cacheMap.find(id) == cacheMap.end()) {
+                return -1; // Key not found
+            } else {
+                // Move the accessed node to the front of the list (most recently used)
+                auto it = cacheMap[id];
+                int value = it->second;
+                cacheList.erase(it);
+                cacheList.push_front({id, value});
+                cacheMap[id] = cacheList.begin();
+                return value;
+            }
+        }
+
+        void putKeyValue(int id, int value) {
+            if (cacheMap.find(id) != cacheMap.end()) {
+                // Key already exists, update the value and move it to the front
+                auto it = cacheMap[id];
+                cacheList.erase(it);
+            } else {
+                // Key does not exist, check if we need to evict the least recently used item
+                if (cacheList.size() == capacity) {
+                    // Evict the least recently used item (the last item in the list)
+                    auto last = cacheList.back();
+                    cacheMap.erase(last.first); // Remove from map
+                    cacheList.pop_back(); // Remove from list
+                }
+            }
+            // Insert the new key-value pair at the front of the list
+            cacheList.push_front({id, value});
+            cacheMap[id] = cacheList.begin(); // Update the map with the new iterator
+        }
 };
